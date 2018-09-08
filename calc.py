839e06c55e0f9728403d02857f4b42ca526cf5e7
Miklós Kondász <miklos.kondasz@gmail.com>
2018-09-08 20:05:33 +0200

    refactorálva

--- calc.py
+++ calc.py
@@ -1,36 +1,27 @@
-while True:
-    try:
-        oper = input("Welcome! Please choose : + - * / or quit with q: ")
-    #Comment 1 komiklos owenlars
-    #Comment 1 OwenLars
-        if oper == "q":
-            break
-        elif oper == "+":
+def calculator():
+    while True:
+        try:
+            operator = input("Welcome! Please choose : + - * / or quit with q: ")
+            if operator == "q":
+                break
             x = float(input("Enter value of x: "))
             y = float(input("Enter value of y: "))
-            result = str(x+y)
+            if operator == "+":
+                result = str(x+y)
+            elif operator == "-":
+                result = str(x-y)
+            elif operator == "*":
+                result = str(x*y)
+            elif operator == "/":
+                result = str(x/y)
+            else:
+                ("Unknown operatoration!")
             print("Result: " + result)
+        except ZeroDivisionError:
+            print("Cannot devide by 0!")
+        except (ValueError, TypeError):
+            print("Error!")
 
-        elif oper == "-":
-            x = float(input("Enter value of x: "))
-            y = float(input("Enter value of y: "))
-            result = str(x-y)
-            print("Result: " + result)
 
-        elif oper == "*":
-            x = float(input("Enter value of x: "))
-            y = float(input("Enter value of y: "))
-            result = str(x*y)
-            print("Result: " + result)
-
-        elif oper == "/":
-            x = float(input("Enter value of x: "))
-            y = float(input("Enter value of y: "))
-            result = str(x/y)
-            print("Result: " + result)
-        else:
-            ("Unknown operation!")
-    except ZeroDivisionError:
-        print("Cannot devide by 0!")
-    except (ValueError, TypeError):
-        print("Error!")
+if __name__ == '__main__':
+    calculator()
